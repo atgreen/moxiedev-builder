1ee7d722719b2cd962d9339b9e6c193420c1ff44
Anthony Green <green@moxielogic.com>
2016-10-28 19:25:23 -0400

    createrepo

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ SRPMDIR=/var/www/html/
 rpmbuild --rebuild $SRPMDIR/moxielogic-moxie-elf-binutils*src.rpm;
 rpmbuild --rebuild $SRPMDIR/moxielogic-moxie-elf-gdb*src.rpm;
 
-find /root/rpmbuild
+mkdir -p /usr/local/MoxieLogic/x86_64
+mv /root/rpmbuild/RPMS/x86_64/* /usr/local/MoxieLogic/x86_64
+createrepo /usr/local/MoxieLogic
 
