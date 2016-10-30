e67baa112b8b55247024c0056604fc1903ecfb39
Anthony Green <green@moxielogic.com>
2016-10-30 14:26:17 -0400

    fix tests

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ mkdir -p $REPODIR/x86_64
 echo "BUILD SCRIPT"
 
 RPMCHECK=`find $REPODIR/x86_64 -name moxielogic-*binutils*`
-if test -z $RPMCHECK; then
+if test -z "$RPMCHECK"; then
 
   rpmbuild --rebuild $SRPMDIR/moxielogic-moxie-elf-binutils*src.rpm;
   rpmbuild --rebuild $SRPMDIR/moxielogic-moxie-elf-gdb*src.rpm;
@@ -17,15 +17,25 @@ if test -z $RPMCHECK; then
 else
 
   RPMCHECK=`find $REPODIR/x86_64 -name bootstrap-*`
-  if test -z $RPMCHECK; then
+  if test -z "$RPMCHECK"; then
 
-    yum install -y moxielogic-moxie-elf-binutils
+    yum install -y moxielogic-moxie-elf-binutils;
     rpmbuild --rebuild $SRPMDIR/bootstrap-moxie-elf-gcc*src.rpm;
     rpm -hiv /root/rpmbuild/RPMS/x86_64/bootstrap*
     rpmbuild --rebuild dist/moxielogic-moxie-elf-newlib*src.rpm;
     mv /root/rpmbuild/RPMS/x86_64/* $REPODIR/x86_64
     mv /root/rpmbuild/RPMS/noarch/* $REPODIR/noarch
-    
+
+  else
+
+      RPMCHECK=`find $REPODIR/x86_64 -name gcc-*`
+      if test -z "$RPMCHECK"; then
+
+	yum install -y moxielogic-moxie-elf-newlib moxielogic-moxie-elf-binutils;
+        rpmbuild --rebuild $SRPMDIR/moxielogic-moxie-elf-gcc*src.rpm;
+	mv /root/rpmbuild/RPMS/x86_64/* $REPODIR/x86_64;
+
+      fi
   fi
 
 fi
